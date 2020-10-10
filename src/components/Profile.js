0ca9a448bc5446247a9bc3e2e1fc22fe3418cdf6
mcay23 <mcay23@ad.unc.edu>
2020-10-10 20:11:09 +0700

    added react router

---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./Profile.css";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className="profile-section">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>Profile page mockup</h1>
+      </div>
+    );
+  }
+}
+
+export default Profile;
