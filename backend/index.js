379e4116784227574ae35025bd4e9290cfd19a1b
pricelev@ad.unc.edu <pricelev@ad.unc.edu>
2020-10-31 21:35:50 -0400

    "adding backend API code"

---
+++ backend/index.js
@@ -0,0 +1,112 @@
+
+
+const Api = require("./ApiServices.js")
+const DB = require("./DBServices.js")
+
+const express = require('express')
+
+const app = express()
+const port = 3001
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+//THE PASSWORD FOR THE DATABASE: A5gkBj0h4m
+//api interface for get player, when request comes in we need to:
+//1: query database for any existing players by that name
+//2: query bungie for non-existing players by that name
+//3: merge results
+//4: return results
+
+app.get('/getPlayer', (req, res) => {
+  const name = req.query.displayName;
+
+  //get the promise from database for search
+  let DBpromise = DB.searchPlayer(name);
+  
+  //get promise from API from search results
+  let Apipromise = Api.findPlayer(name);
+
+
+    Promise.all([DBpromise,Apipromise]).then(data => {
+    let dbResult = data[0];
+    let apiResults = data[1].data.Response;
+
+    let allPlayers = [];
+    for(rows of dbResult){
+      let temp = {
+        DisplayName: rows.DisplayName,
+        MembershipID: rows.MembershipID,
+        Emblem: "https://www.bungie.net/common/destiny2_content/icons/486c1483be15aabd1ef7adb7a87c7a72.jpg",
+        xbox: rows.Xbox,
+        psn: rows.PSN,
+        steam: rows.Steam
+      }
+      allPlayers.push(temp);
+    }
+    for(rows of apiResults){
+      let temp = {
+        DisplayName: rows.displayName,
+        MembershipID: rows.membershipId,
+        Emblem: "https://www.bungie.net/common/destiny2_content/icons/486c1483be15aabd1ef7adb7a87c7a72.jpg",
+        xbox: 0,
+        psn: 0,
+        steam: 0
+      }
+      if(rows.membershipType ==3){
+        temp.steam = 1;
+      }
+      allPlayers.push(temp);
+    }
+    
+    res.send(allPlayers);
+  }).catch((error) => {
+    console.log(error);
+  });  
+  
+} );
+
+
+/*Api method for client to request a player update
+  Requires membership id param
+
+*/
+app.get('/updatePlayer', (req, res) => {
+  const memID = req.query.membershipId + '';
+  const memType = req.query.membershipType;
+
+  if(!memID || !memType){
+    throw new Error('REQUIRED PARAMETER MISSING')
+  }
+ 
+  DB.updatePlayer(memID,memType).then(data => res.sendStatus(data))
+ // res.send("player updated");
+  
+  
+ 
+  
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+app.get("/reportCard" , (req,res)=>{
+  const memID = req.query.membershipId;
+  if(!memID){
+    throw new Error('REQUIRED PARAMETER MISSING')
+  }
+
+  DB.getReportCard(memID).then(data => res.send(data));
+
+
+
+})
+
+
