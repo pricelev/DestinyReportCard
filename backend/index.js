e70c2177b0ed1dc7e7374f813e05ec21b711f5ad
Suhas Dundigalla <d.suhas.4599@gmail.com>
2020-11-20 14:11:34 -0500

    Reverting to no cors in backend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,9 @@ const Api = require("./ApiServices.js");
 const DB = require("./DBServices.js");
 
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 const port = 3001;
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
