f78c75eedec737789b1d19b522d5dc1806c88b21
Suhas Dundigalla <d.suhas.4599@gmail.com>
2020-11-21 10:09:25 -0500

    Cors Origin updated for live

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const saltRounds = 10;
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://www.destinyreportcard.com:3000"],
   methods: ["GET", "POST"],
   credentials: true,
 }));
