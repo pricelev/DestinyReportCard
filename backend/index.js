cbf691c30878dcda5437960460132ca3f476ab37
Suhas Dundigalla <d.suhas.4599@gmail.com>
2020-11-22 13:55:57 -0500

    Added Access-Control-Allow-Origin: to cors

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors({
   origin: ["http://destinyreportcard.com"],
+  Access-Control-Allow-Origin: ["http://destinyreportcard.com"],
   //origin: ["http://localhost:3000"],
   methods: ["GET", "POST"],
   credentials: true,
