46e3efe6086377d49bcc6f8160f24c1ba4a2aa66
mcay23 <mcay23@ad.unc.edu>
2020-11-23 22:15:16 +0700

    added documentation to nav

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Destiny Tracker
 
 This is a web app for generating report card statistics for Destiny 2 players.
-Look up your name to get an aggregate 'score' for your character. Create a profile 
+Look up your name to get an aggregate 'score' for your character. Create a profile
 for quick access to your most important Destiny 2 data and
 follow other players to keep track of their statistics.
 
@@ -14,6 +14,10 @@ Marcel Yuwono, Levy Price, Austin McGee, Suhas Dundigalla
 http://www.destinyreportcard.com/
 
 http://ec2-3-133-157-251.us-east-2.compute.amazonaws.com:3000/
+
+## Documentation
+
+http://www.destinyreportcard.com/api/
 
 ## Attribution
 
--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
 import "./Topnav.css";
 import { Link } from "react-router-dom";
@@ -13,16 +12,13 @@ function Topnav() {
   const logoutAPI = "http://www.destinyreportcard.com:3001/logout";
   const loginAPI = "http://www.destinyreportcard.com:3001/login";
 
-
-
   const [loginStatus, setloginStatus] = useState(false);
 
   Axios.defaults.withCredentials = true;
   let history = useHistory();
 
   const logout = () => {
-    Axios.get(logoutAPI, {
-    }).then((response) => {
+    Axios.get(logoutAPI, {}).then((response) => {
       console.log(response);
     });
   };
@@ -32,10 +28,9 @@ function Topnav() {
   useEffect(() => {
     Axios.get(loginAPI).then((response) => {
       console.log(response);
-      if (response.data.loggedIn == true){
+      if (response.data.loggedIn == true) {
         setloginStatus(true);
-      }
-      else {
+      } else {
         setloginStatus(false);
       }
     });
@@ -50,26 +45,34 @@ function Topnav() {
           <Nav.Link href="/" id="nav-item-white">
             Home
           </Nav.Link>
-          { loginStatus == true &&
-          <Nav.Link href="/profile" id="nav-item-white">
-            Profile
+          {loginStatus == true && (
+            <Nav.Link href="/profile" id="nav-item-white">
+              Profile
+            </Nav.Link>
+          )}
+          <Nav.Link
+            href="http://destinyreportcard.com/api/"
+            id="nav-item-white"
+          >
+            Documentation
           </Nav.Link>
-          }
         </Nav>
         <Nav>
-          { loginStatus == false &&
-          <Link to="/login">
-            <Button className="nav-button">Login</Button>
-          </Link>
-          }
-          { loginStatus == false &&
-          <Link to="/signup">
-            <Button className="nav-button">Signup</Button>
-          </Link>
-          }
-          { loginStatus == true &&
-            <Button className="nav-button" onClick={logout}>Logout</Button>
-          }
+          {loginStatus == false && (
+            <Link to="/login">
+              <Button className="nav-button">Login</Button>
+            </Link>
+          )}
+          {loginStatus == false && (
+            <Link to="/signup">
+              <Button className="nav-button">Signup</Button>
+            </Link>
+          )}
+          {loginStatus == true && (
+            <Button className="nav-button" onClick={logout}>
+              Logout
+            </Button>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
