3f47b0bec535564da492db9bbd15269bdba0ad64
Suhas Dundigalla <d.suhas.4599@gmail.com>
2020-11-23 22:26:35 -0500

    Get logged in user data

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,7 +7,87 @@ import ProfileChart from "./ProfileChart";
 import ReportSummary from "./ReportSummary";
 import CharacterPanel from "./CharacterPanel";
 
+const loginAPI = "http://www.destinyreportcard.com:3001/login";
+const API = "http://www.destinyreportcard.com:3001/reportCard/?membershipId=";
+const CORS = "https://cors-anywhere.herokuapp.com/";
+
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      memType: this.props.match.params.membershipType,
+      memId: this.props.match.params.membershipId,
+      profileData: {},
+      displayName: "",
+      profPic: "/profile.jpg",
+      loginStatus: false,
+    };
+  }
+
+  componentDidMount() {
+    Axios.get(loginAPI).then((response) => {
+      if (response.data.loggedIn == true) {
+        this.setState({
+          loginStatus: true,
+          memType: response.data.user[0].membershipType,
+          memId: response.data.user[0].membershipId,
+        });
+        fetch(
+          CORS + API + this.state.memId + "&membershipType=" + this.state.memType
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (data) {
+              if (data.characters.characterInfo.length === 3) {
+                let character1 = data.characters.characterInfo[0];
+                let character2 = data.characters.characterInfo[1];
+                let character3 = data.characters.characterInfo[2];
+                if (character1.LightLevel >= character2.LightLevel) {
+                  if (character1.LightLevel >= character3.LightLevel) {
+                    this.setState({
+                      profPic: character1.emblemIcon,
+                    });
+                  } else {
+                    this.setState({
+                      profPic: character3.emblemIcon,
+                    });
+                  }
+                } else if (character2.LightLevel >= character3.LightLevel) {
+                  this.setState({
+                    profPic: character2.emblemIcon,
+                  });
+                } else {
+                  this.setState({
+                    profPic: character3.emblemIcon,
+                  });
+                }
+              } else {
+                this.setState({
+                  profPic: data.characters.characterInfo[0].emblemIcon,
+                })
+              }
+              
+              this.setState({
+                isLoaded: true,
+                profileData: data,
+                displayName: data.playerInfo.DisplayName.value,
+              });
+            }
+            console.log(data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      else {
+        this.setState({
+          loginStatus: false,
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="profile-container">
